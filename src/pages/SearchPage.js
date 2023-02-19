666bb46077c2cd2a1a1a5551598025d0ff4939fa
pedrovega1 <luidzhi.vegas@mail.ru>
2023-02-19 13:48:09 +0600

    appi

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -33,9 +33,9 @@ export default function SearchPage(props) {
   return (
     <>
     <div className="rounded-md  bg-white mx-10 p-6 -mt-6 shadow-lg">
-      <div className="border-b-2 border-gray-100 flex justify-between">
-        <div className=" flex w-1/2 flex-start text-gray-500 cursor-pointer ml-2 ">
-          <div className="flex space-x-4  ">
+      <div className="flex flex-col">
+        <div className=" border-b-2 border-gray-100 flex justify-between">
+          <div className="flex w-1/2 flex-start text-gray-500 space-x-3  ">
           <a
             className={searchMode === "hotel" ? "  border-b-4 border-indigo-500 text-gray-700 font-bold " : ""}
             onClick={() => {
@@ -75,43 +75,54 @@ export default function SearchPage(props) {
             Car Rental
           </a>
    </div>
-        </div>
+       
         <div className="flex flex-end w-1/s">
         <RoundDripDropdown/>
         <PassengerDropdown/>
         </div>
-      </div>
 
+      </div>
       <div className="flex">
-      <div className="grid grid-cols-2 gap-2 relative w-fit h-full mr-1">
-      <div className="flex flex-col">
-      <label className="text-sm text-gray-600 font-semibold">
+            <div className="grid grid-cols-2 gap-2 relative w-fit h-full mr-1">
+              <div className="flex flex-col">
+                <label className="text-sm text-gray-600 font-semibold">
                   Leaving From
                 </label>
                 <LocationDropdown
                   handleLocationChange={handleDeparuteChange}
                   location="departure"
                 />
-      </div>
-      </div>
-            <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1 rounded-full border p-1 border-gray-400 z-10 bg-white hover:bg-gray-100">
-            <SwitchHorizontalIcon className="h-4 text-gray-700" />
-            </button>
-            <div className="flex flex-col">
-            <label className="text-sm text-gray-600 font-semibold">
-                  Check out
+              </div>
+              <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1 rounded-full border p-1 border-gray-400 z-10 bg-white hover:bg-gray-100">
+                <SwitchHorizontalIcon className="h-4 text-gray-700" />
+              </button>
+              <div className="flex flex-col">
+                <label className="text-sm text-gray-600 font-semibold">
+                  Going To
                 </label>
                 <LocationDropdown
-                handleLocationChange={handleArrivalChange}
-                location="arrival"
-              />
+                  handleLocationChange={handleArrivalChange}
+                  location="arrival"
+                />
+              </div>
             </div>
-      </div>
-     <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1 rounded-full border p-1 border-gray-400 z-10 bg-white hover:bg-gray-100">
-       <SwitchHorizontalIcon className="h-4 text-gray-700" />
-     </button >
-            <div className="flex flex-col">
-            <label className="text-sm text-gray-600 font-semibold">
+            <div className="flex relative ml-1">
+              <div className="flex flex-col">
+                <label className="text-sm text-gray-600 font-semibold">
+                  Check in
+                </label>
+                <ReactDatePicker
+                  className="border border-gray-300 rounded-md py-2 px-4 w-64 mr-1 bg-gray-100"
+                  selected={startDate}
+                  onChange={(date) => setStartDate(date)}
+                />
+              </div>
+
+              <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1 rounded-full border p-1 border-gray-400 z-10 bg-white hover:bg-gray-100">
+                <SwitchHorizontalIcon className="h-4 text-gray-700" />
+              </button>
+              <div className="flex flex-col">
+                <label className="text-sm text-gray-600 font-semibold">
                   Check out
                 </label>
                 <ReactDatePicker
@@ -119,16 +130,20 @@ export default function SearchPage(props) {
                   selected={endDate}
                   onChange={(date) => setEndDate(date)}
                 />
+              </div>
             </div>
-            <div className="flex">
+          </div>
+      </div>
+      <div className="flex">
           <button
             onClick={handleSearchClick}
             className="text-white bg-blue-600 rounded-md py-4 px-8 w-fit h-fit mt-auto hover:bg-blue-700 active:bg-blue-800"
           >
             Search
           </button>
-          </div>
-    </div>
+        </div>
+
+  </div>
   </>
   );
 }
